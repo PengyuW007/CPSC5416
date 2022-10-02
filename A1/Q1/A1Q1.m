05342e2331446044447155742296980a88bc8f92
Skadoosh777 <670641576@qq.com>
2022-10-01 21:32:46 -0400

    Update A1Q1.m

--- A1/Q1/A1Q1.m
+++ A1/Q1/A1Q1.m
@@ -3,7 +3,17 @@
 x=0:250;
 y = 1704*sin(pi*x/255);
 nbins = 25;
-
 bar(x,y);
 xlabel('x');
 ylabel('Frequency');
+
+%if we want to make it monotonic, we must move it to right, which is as
+%shown below
+
+figure;
+
+h = 1704*sin(pi*x/255/2);
+
+bar(x,h);
+xlabel('x');
+ylabel('Frequency h');
