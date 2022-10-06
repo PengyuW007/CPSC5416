d91f50f9b2c94a6e78afd53be09804ee9fc77987
Skadoosh777 <670641576@qq.com>
2022-10-06 11:25:55 -0400

    q2 finished

--- A1/Q2/A1Q2.m
+++ A1/Q2/A1Q2.m
@@ -1,25 +1,41 @@
-k = 0:1:7;
+k = 0:7;
 nk = [1028 3544 5023 3201 1867 734 604 383];
-n = 64*64; % since this is a 3 bit image, 8 gray levels, 0->7, 000->111
 
 bar(k,nk);
 xlabel('Gray value');
 ylabel('#pixels');
 title('Original');
 
-% Normalized
-rk=k/7;
-prk = nk/n;
-t = [];
-t(1)= prk(1);
-
-for k=2:1:8
-    t(k)= prk(k)+t(k-1);
+% Total number of 
+n = [];
+n(1) = nk(1);
+for l =2:8
+    n(l)=n(l-1)+nk(l);
 end
 
-s = t*7;
+N = n(8);% Total number of pixels
 
+t = [];
+t =round(n/N,2);
+figure;
+bar(k/7,t);
+xlabel('Normalized gray value');
+ylabel('Fraction of #pixels');
+title('Normalized');
+%disp(t);
+s = round(n/N*7);
+%disp(s);
 
+sk = mat2cell(sort(s),1,histcounts(findgroups(s)));
+[r,c]=size(sk);
+%fprintf("%d\n",sk{1,6});
 
-disp(t);
-disp(s);
+Sk = [1028 0 3544  0 5023 3201 1867 734+604+383 ];
+Pk = round(Sk/N,2);
+%disp(Pk);
+
+figure;
+bar(k,Pk);
+xlabel('Gray value');
+ylabel('#pixels');
+title("Histogram of output image");
