8078bb3d8cc84169fa6ce29ad7ac6d8f920e8aff
Skadoosh777 <670641576@qq.com>
2022-10-12 11:37:47 -0500

    applying transformation

--- A1/Q1/A1Q1.m
+++ A1/Q1/A1Q1.m
@@ -1,34 +1,43 @@
-x=0:255;
-y = 1704*sin(pi*x/255);
-bar(x,y);
-xlabel('x');
-ylabel('Frequency h1');
-
-%y H(D)=1704sin(πD/ 255)
-figure;
-H = histeq(y);
-imhist(H);
 %get L, total number of gray levels, 8 bits
 L = 2^8; % 256 grey levels, so it needs to be divided into 256 bins
 
-% figure;
-% H =1704*sin(pi*x/255)/1704;
-% bar(x,H);
-% 
-% % step 0,
-% %get L, total number of gray levels, 8 bits
-% L = 2^8; % 256 grey levels, so it needs to be divided into 256 bins
-% % get n, total number of pixels
-% n =[];
-% n(1)= round(1704*sin(pi*0/255));
-% t=[];
-% t(1) = n(1);
-% for d = 2:256
-%     n(d) = round(1704*sin(pi*(d-1)/255));
-%     t(d) = t(d-1)+n(d);
-% end
-% N = t(256);% total number of pixels
-% pIn = [];
-% pIn = n/N;
-% disp(pIn);
-% %fprintf("d: %d, v: %d, N: %d\n",d,round(1704*sin(pi*0/255)),N);
+% H(D)=1704sin(πD/ 255)
+x=0:(L-1);
+y = 1704*sin(pi*x/255);
+bar(x,y);
+xlabel('Gray value');
+ylabel('Frequency h1');
+title("Histogram of input image");
+
+% step 0,
+% get n, total number of pixels
+h = @(x) 1704*sin(pi*x/255);
+N = round(integral(h,0,255));
+
+% get each gray level #pixels
+n = zeros(1, 256);
+for i =1:L
+    n(i) = round(1704*sin(pi*(i-1)/255));
+    %fprintf("i: %d, %d\n",i-1,n(i));
+end
+
+% Applying the transforamtion
+t = zeros(1,256);
+t(1)=n(1);
+for i = 2:L
+    t(i)=n(i)+t(i-1);
+    %fprintf("i: %d, %d\n",i-1,t(i));
+end
+%disp(t);
+fprintf("\n");
+s =zeros(1,256);
+for i = 1:L
+    p = t(i)/N;
+    index = round(p*(L-1));
+    s(index+1)=s(index+1)+1;
+end
+figure;
+bar(x,s);
+xlabel('Gray value');
+ylabel('#pixels');
+title("Histogram of output image");
