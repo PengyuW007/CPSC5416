58eb6e49c10d4ca66ad8250368dc9a59d032a08d
Skadoosh777 <670641576@qq.com>
2022-10-16 17:22:43 -0400

    Find some issues and fixed in q3

--- A1PengyuWang0425157/Q3/A1Q3.m
+++ A1PengyuWang0425157/Q3/A1Q3.m
@@ -8,6 +8,7 @@ title('Specified histogram');
 
 n = [0 1365 2731 4096 4096 2731 1365 0];
 % Total number of pixels
+[row,L] = size(n);
 N = 0;
 for i =1:L
     N=N+n(i);
@@ -33,6 +34,12 @@ t(1)=n(1);
 for i=2:L
     t(i)=n(i)+t(i-1);
 end
+s = zeros(1,L);
+for i = 1:L
+    p = t(i)/N;
+    index = round(p*(L-1));
+    s(index+1)=s(index+1)+1;
+end
 
 % Equalization output histogram
 S =zeros(1,L);
@@ -50,10 +57,25 @@ for i=1:L
 end
 
 figure;
-bar(k,S);
+bar(x,S);
 xlabel('Gray value');
 ylabel('#pixels');
 title('Histogram of Equalization output image');
+
+f = figure;
+col={'r','n'};
+dat = [];
+for i = 1:L
+    for j = 1:2
+        if(j==1)
+            dat(i,j) = x(i);
+        elseif(j==2)
+            dat(i,j) = S(i);
+        end
+    end
+end
+            
+uitable(f,'ColumnName',col,'Position',[100 100 170 170],'Data',dat);
 
 %%%%%%%%%%%%%
 % Specified %
