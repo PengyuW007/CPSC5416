3059b881a1d10e1fb134d12beb7dc2601ab92752
Skadoosh777 <670641576@qq.com>
2022-10-28 19:10:33 -0400

    refactor ILPF and IHPF

--- A2PengyuWang0425157/Q2/IdealPassFilters.m
+++ A2PengyuWang0425157/Q2/IdealPassFilters.m
@@ -20,68 +20,33 @@ D = sqrt(U.^2+V.^2);
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Ideal Low Pass Filter(ILPF) %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% case 1, d0_1 = 5;
-dLow0_1 = 5; % D0
-HLow1 = zeros(r2,c2);
-for i=1:r2
-    for j = 1:c2
-        if(D(i,j)<=dLow0_1)
-            HLow1(i,j) = 1;
+
+% Put all the D0s in to an array
+dLow0 = [5 80 230];
+[rowsLow,lenLow] =size(dLow0);
+index = 4;
+for t = 1:lenLow
+    dLow0_i = dLow0(t); % D0
+    HLowi = zeros(r2,c2);
+    for i=1:r2
+        for j = 1:c2
+            if(D(i,j)<=dLow0_i)
+                HLowi(i,j) = 1;
+            end
         end
     end
+    GLowi = ft.*HLowi; % G(u,v)
+
+    igLow0_i = ifft2(fftshift(GLowi)); % Inverse of G(DFT)
+    % resize it to normal size, put into middle of page
+    igLowi = igLow0_i(1:r,1:c);
+    igLowi = real(igLowi); % real part of function G
+
+    subplot(2,3,index);
+    index = index+1;
+    imshow(igLowi);
+    title("After ILPF when D0="+dLow0_i);
 end
-GLow1 = ft.*HLow1; % G(u,v)
-
-igLow0_1 = ifft2(fftshift(GLow1)); % Inverse of G(DFT)
-% resize it to normal size, put into middle of page
-igLow1 = igLow0_1(1:r,1:c);
-igLow1 = real(igLow1); % real part of function G
-
-subplot(2,3,4);
-imshow(igLow1);
-title('After ILPF when D0=5');
-
-% case 2, d0_2 = 30;
-dLow0_2 = 30; % D0
-HLow2 = zeros(r2,c2);
-for i=1:r2
-    for j = 1:c2
-        if(D(i,j)<=dLow0_2)
-            HLow2(i,j) = 1;
-        end
-    end
-end
-GLow2 = ft.*HLow2; % G(u,v)
-
-igLow0_2 = ifft2(fftshift(GLow2)); % Inverse of G(DFT)
-% resize it to normal size, put into middle of page
-igLow2 = igLow0_2(1:r,1:c);
-igLow2 = real(igLow2); % real part of function
-
-subplot(2,3,5);
-imshow(igLow2);
-title('After ILPF when D0=30');
-
-% case 3, d0_3 = 30;
-dLow0_3 = 230; % D0
-HLow3 = zeros(r2,c2);
-for i=1:r2
-    for j = 1:c2
-        if(D(i,j)<=dLow0_3)
-            HLow3(i,j) = 1;
-        end
-    end
-end
-GLow3 = ft.*HLow3; % G(u,v)
-
-igLow0_3 = ifft2(fftshift(GLow3)); % Inverse of G(DFT)
-% resize it to normal size, put into middle of page
-igLow3 = igLow0_3(1:r,1:c);
-igLow3 = real(igLow3); % real part of function
-
-subplot(2,3,6);
-imshow(igLow3);
-title('After ILPF when D0=230');
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
