f56246dc3d918ad6d9aa317ce005adeced06d264
Skadoosh777 <670641576@qq.com>
2022-10-31 20:53:39 -0400

    Update livingrooms.m

--- A2PengyuWang0425157/Q3/livingrooms.m
+++ A2PengyuWang0425157/Q3/livingrooms.m
@@ -17,7 +17,25 @@ ft = fftshift(fft2(livingroom,r2,c2));
 figure;
 imshow(mat2gray(log(1+abs(ft))));
 
-u = -c:c-1;
-v = -r:r-1;
+% u = -c:c-1;
+% v = -r:r-1;
+% 
+% [U,V] = meshgrid(u,v);
+% D = sqrt(U.^2+V.^2);
 
-[U,V] = meshgrid(u,v);
+% DLow0 = 50;
+% HLow = zeros(r2,c2);
+% Ones = ones(r2,c2);
+% for i = 1:r2
+%     for j = 1:c2
+%         HLow(i,j)=Ones(i,j)-exp(-0.5.*((D(i,j).^2)-(DLow0.^2))./(D(i,j).*5));
+%     end
+% end
+% GLow = ft.*HLow;
+% 
+% igLow0=ifft2(fftshift(GLow));
+% igLow=igLow0(1:r,1:c);
+% igLow = real(igLow);
+% 
+% figure;
+% imshow(igLow);
