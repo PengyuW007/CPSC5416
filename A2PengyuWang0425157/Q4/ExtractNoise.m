d8fc45c7a118a8414acf4a739cad891c1338b24a
Skadoosh777 <670641576@qq.com>
2022-11-05 17:06:38 -0400

    Q4 ab finished

--- A2PengyuWang0425157/Q4/ExtractNoise.m
+++ A2PengyuWang0425157/Q4/ExtractNoise.m
@@ -36,6 +36,56 @@ for i = 1:y
         freqBlack(round(value*256)+1)=freqBlack(round(value*256)+1)+1;
     end
 end
+
+[q,lenFreqBlack] = size(freqBlack);
+pro =zeros(1,lenFreqBlack);
+for i =1:lenFreqBlack
+    pro(i) = freqBlack(i)/N;
+end
 figure;
-bar(freqBlack);
-title('Gaussian/Erlang noise frequency histogram');
+bar(1:256,pro);
+title('Gaussian(Erlang) noise PDF');
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Add new noise to blank image %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+sum = 0;
+count = zeros(1,N);
+index = 1;
+for i = 1:y
+    for j = 1:x
+        value = roiBlack(i,j);
+        count(index) = round(value*256);
+        sum = count(index)+sum;
+        index = index+1;
+    end
+end
+mean = round(sum/N,2);
+% fprintf("%f",mean);
+varSum = 0;
+for i = 1:N
+    varSum = (count(i) -mean)^2+varSum;
+end
+var = round(varSum/N,2);
+blankImg = ones(32,32);
+noisyImg = imnoise(blankImg,"gaussian",mean,var);
+figure;
+subplot(1,2,1);
+imshow(noisyImg);
+title("Gaussian noise with mean = " + mean +", var = "+var);
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Noisy image 2 histogram %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%
+noiseArr = zeros(1,N);
+index = 1;
+for i = 1:y
+    for j = 1:x
+        valueNoise = noisyImg(i,j);
+        noiseArr(index) =round(valueNoise*256);
+        inidex = index+1;
+    end
+end
+subplot(1,2,2);
+hist(noiseArr,256);
+title("Histogram of Gaussian noise to blank with mean = " + mean +", var = "+var);
