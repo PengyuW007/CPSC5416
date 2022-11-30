f2334198da26a6f29e1a10ebac35e021f3b77c39
Skadoosh777 <670641576@qq.com>
2022-11-30 18:51:31 -0500

    Q1 done

--- A3PengyuWang0425157/Q1/Device.m
+++ A3PengyuWang0425157/Q1/Device.m
@@ -3,17 +3,20 @@ close all;
 clc;
 
 device = imread("device.pgm");
-subplot(1,3,1);
-imshow(device);
-title('Device input image')
 
-SE = strel('disk',10);
-eroseDevice = imerode(device,SE);
-subplot(1,3,2);
-imshow(eroseDevice);
-title('Device after erosion')
+for i = 1:5
+    subplot(5,3,3*i-2);
+    imshow(device);
+    title('Device input image')
 
-closeDevice = imclose(device,SE);
-subplot(1,3,3);
-imshow(closeDevice);
-title('Device after closing')
+    SE = strel('disk',i*10);
+    eroseDevice = imerode(device,SE);
+    subplot(5,3,3*i-1);
+    imshow(eroseDevice);
+    title("Device after erosion, size = "+i*10);
+
+    closeDevice = imclose(device,SE);
+    subplot(5,3,3*i);
+    imshow(closeDevice);
+    title("Device after closing, size = "+i*10);
+end
