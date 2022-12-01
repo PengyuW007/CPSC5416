ea3b2cb02f7068c5f24db5c34d6c4127291dcc1e
Skadoosh777 <670641576@qq.com>
2022-12-01 11:50:17 -0500

    Update Fly.m

--- A3PengyuWang0425157/Q2/Fly.m
+++ A3PengyuWang0425157/Q2/Fly.m
@@ -16,9 +16,50 @@ subplot(3,3,2);
 imshow(erodeFly);
 title("Fly after erosion");
 
-erodeFly2 = imerode(erodeFly,SE2); 
-boundaryErode = erodeFly - erodeFly2;
+erodeFly1 = imerode(erodeFly,SE2); 
+boundaryErode = erodeFly - erodeFly1;
 subplot(3,3,3);
 imshow(boundaryErode);
 title("Erosion boundary");
 
+% Opening
+subplot(3,3,4);
+imshow(fly);
+title("Fly original image");
+openFly = imopen(fly,SE);
+subplot(3,3,5);
+imshow(openFly);
+title("Fly after opening");
+
+erodeFly2 = imerode(openFly,SE2);
+boundaryOpen = openFly - erodeFly2;
+subplot(3,3,6);
+imshow(boundaryOpen);
+title("Opening boundary");
+
+% Closing
+subplot(3,3,7);
+imshow(fly);
+title("Fly original image");
+closeFly = imopen(fly,SE);
+subplot(3,3,8);
+imshow(closeFly);
+title("Fly after closing");
+
+erodeFly3 = imerode(closeFly,SE2);
+boundaryClose = closeFly - erodeFly3;
+subplot(3,3,9);
+imshow(boundaryClose);
+title("Closing boundary");
+
+figure;
+imshow(boundaryErode);
+title("Erosion boundary");
+
+figure;
+imshow(boundaryOpen);
+title("Opening boundary");
+
+figure;
+imshow(boundaryClose);
+title("Closing boundary");
