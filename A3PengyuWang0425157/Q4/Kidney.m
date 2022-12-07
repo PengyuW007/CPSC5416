7f4b2460031799a95ce17db127aeb90224686b43
Skadoosh777 <670641576@qq.com>
2022-12-07 16:04:22 -0500

    Q4 done

--- A3PengyuWang0425157/Q4/Kidney.m
+++ A3PengyuWang0425157/Q4/Kidney.m
@@ -4,15 +4,50 @@ clc;
 
 kidney = imread("kidney_original.tif");
 kidney = im2double(kidney);
-subplot(1,3,1);
-imshow(kidney);
-title("Original kidney image");
 
 % Got gradient magnitude and direction
+figure;
 [Gmag,Gdir] = imgradient(kidney,'sobel');
-subplot(1,3,2);
+subplot(4,2,1);
 imshow(Gmag);
 title("Gradient magnitude");
-subplot(1,3,3);
+subplot(4,2,2);
+imhist(Gmag);
+subplot(4,2,3);
 imshow(Gdir);
 title("Gradient direction");
+subplot(4,2,4);
+imhist(Gdir);
+
+[Gx,Gy] = imgradientxy(kidney);
+subplot(4,2,5);
+imshow(Gx);
+title("Gx");
+subplot(4,2,6);
+imhist(Gx);
+subplot(4,2,7);
+imshow(Gy);
+title("Gy");
+subplot(4,2,8);
+imhist(Gy);
+
+figure;
+subplot(1,2,1);
+imshow(kidney);
+title("Original kidney");
+subplot(1,2,2);
+imshow(Gmag);
+title("Filtered kidney");
+
+figure;
+% Choosing threshold
+for i = 0:0.01:0.09
+    T = i;
+    Edgei = max(Gmag,T);
+    Edgei(Edgei==round(T))=0;
+
+    Edgei = imbinarize(Edgei);
+    subplot(2,5,i*100+1);
+    imshow(Edgei);
+    title("T = "+T);
+end
